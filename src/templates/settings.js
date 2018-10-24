f8bd3b9385d15f86f3298c2892da533204a177ef
Aaron <azza1990uk@yahoo.com>
2018-10-24 02:26:37 +0100

    Add settings page

---
+++ src/templates/settings.js
@@ -0,0 +1,73 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { kebabCase } from "lodash";
+import Helmet from "react-helmet";
+import { graphql, Link } from "gatsby";
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
+
+export const SettingsTemplate = ({
+  content,
+  contentComponent,
+  description,
+  featuredimage,
+  title,
+  helmet
+}) => {
+  const PostContent = contentComponent || Content;
+
+  return (
+    <div>
+      <PostContent content={content} />
+    </div>
+  );
+};
+
+SettingsTemplate.propTypes = {
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  description: PropTypes.string,
+  featuredimage: PropTypes.string,
+  title: PropTypes.string,
+  helmet: PropTypes.instanceOf(Helmet)
+};
+
+const Settings = ({ data }) => {
+  const { markdownRemark: post } = data;
+
+  return (
+    <Layout>
+      <SettingsTemplate
+        content={post.html}
+        contentComponent={HTMLContent}
+        description={post.frontmatter.description}
+        featuredimage={post.frontmatter.featuredimage}
+        helmet={<Helmet title={`${post.frontmatter.title}`} />}
+        title={post.frontmatter.title}
+      />
+    </Layout>
+  );
+};
+
+Settings.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object
+  })
+};
+
+export default Settings;
+
+export const pageQuery = graphql`
+  query SettingsByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        description
+        featuredimage
+      }
+    }
+  }
+`;
