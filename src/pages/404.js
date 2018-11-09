eaba16e023a6f5b84eda3782fb0056dfb9e13aae
Aaron <azza1990uk@yahoo.com>
2018-11-09 14:39:12 +0000

    Add element wrapper on 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import AaBlockquote from "../components/AaBlockquote/AaBlockquote";
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
+    <div className="element">
       <AaBlockquote quote="Oops, this page doesn't exist, please contact us if you can't find what you're looking for" />
     </div>
   </Layout>
