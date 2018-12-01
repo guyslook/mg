485efe00a925081f507e10ee18843ef6076960ff
Aaron <azza1990uk@yahoo.com>
2018-12-01 22:56:21 +0000

    Remove product template files and references

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,5 @@
 import CMS from "netlify-cms";
 
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
-import ProductPagePreview from "./preview-templates/ProductPagePreview";
 
-CMS.registerPreviewTemplate("products", ProductPagePreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
