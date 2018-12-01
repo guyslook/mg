0d904431bbdb65e1216ef61dbe6058739274cc36
Aaron <azza1990uk@yahoo.com>
2018-12-01 19:10:34 +0000

    Fix the links on services

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -13,31 +13,31 @@ class Services extends React.Component {
       <div className="services-links">
         <div className="service-links-inner">
           <div className="twenty turning">
-            <a href="/services/turning">
+            <a href="/turning">
               <img width="200" src={turning} alt="Turning" />
               <div>Turning</div>
             </a>
           </div>
           <div className="twenty milling">
-            <a href="/services/milling">
+            <a href="/milling">
               <img width="200" src={milling} alt="Milling" />
               <div>Milling</div>
             </a>
           </div>
           <div className="twenty welding">
-            <a href="/services/welding">
+            <a href="/welding">
               <img width="200" src={welding} alt="Welding" />
               <div>Welding</div>
             </a>
           </div>
           <div className="twenty repair">
-            <a href="/services/repair-work">
+            <a href="/repair-work">
               <img width="200" src={repair} alt="Repair Work" />
               <div>Repair Work</div>
             </a>
           </div>
           <div className="twenty twentyone bespoke">
-            <a href="/services/bespoke-parts">
+            <a href="/bespoke-parts">
               <img width="200" src={bespoke} alt="Bespoke parts" />
               <div>Bespoke parts</div>
             </a>
