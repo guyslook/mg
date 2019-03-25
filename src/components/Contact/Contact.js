5539636c535768e26d3ca4a54499ec4a723fcbbf
Aaron <azza1990uk@yahoo.com>
2019-03-25 22:14:45 +0000

    Add new contact component

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+require("./Contact.scss");
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="contact">
+          <div className="contactform" />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Contact;
