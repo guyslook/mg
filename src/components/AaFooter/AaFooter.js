771f7cd8c83d8964e21e6ef327a5feec9c759205
Aaron <azza1990uk@yahoo.com>
2019-05-20 03:24:29 +0100

    Styles for footer and contact form

--- src/components/AaFooter/AaFooter.js
+++ src/components/AaFooter/AaFooter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import { FaFacebook, FaTwitter, FaInstagram, FaPhone, FaMap, FaEnvelope  } from "react-icons/fa";
 import ReactHtmlParser, {
   processNodes,
   convertNodeToElement,
@@ -23,8 +23,15 @@ class AaFooter extends React.Component {
     );
     return (
       <footer className="footer">
-        <div className="footerAddress">{ReactHtmlParser(newAddressHTML)}</div>
-        <div className="footerSocial">
+        <div className="footerAddress hidden">{ReactHtmlParser(newAddressHTML)}</div>
+        <footer class="ftco-footer ftco-section">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <div class="ftco-footer-widget is-4">
+              <h2 class="ftco-heading-2">Environ Digital</h2>
+              <p>Helping businesses succeed online</p>
+              <div className="footerSocial">
           <ul>
             {this.props.socialLinks.facebook ? (
               <li>
@@ -55,7 +62,49 @@ class AaFooter extends React.Component {
             )}
           </ul>
         </div>
-        {/* <div className="footerSocial">{this.props.socialLinks}</div> */}
+            </div>
+          </div>
+          <div class="column">
+            <div class="ftco-footer-widget col-4">
+              <h2 class="ftco-heading-2">Our Services</h2>
+              <ul class="list-unstyled">
+                <li><a href="/website-design-and-development" class="py-2 d-block">Websites</a></li>
+                <li><a href="/social-media-setup-and-content-creation" class="py-2 d-block">Social media</a></li>
+                <li><a href="/training-and-workshops" class="py-2 d-block">Training</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="column">
+             <div class="ftco-footer-widget col-4">
+              <h2 class="ftco-heading-2">Links</h2>
+              <ul class="list-unstyled">
+                <li><a href="/" class="py-2 d-block">Home</a></li>
+                <li><a href="/about" class="py-2 d-block">About</a></li>
+                <li><a href="/our-personal-approach" class="py-2 d-block">Approach</a></li>
+                <li><a href="/blog" class="py-2 d-block">Blog</a></li>
+                <li><a href="/contact-us" class="py-2 d-block">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="column">
+            <div class="ftco-footer-widget mb-4">
+            	<h2 class="ftco-heading-2">Have a Questions?</h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><FaMap size="1em" /><span class="text"></span></li>
+	                <li><FaPhone size="1em" /><span class="text"> +447807042123</span></li>
+	                <li><a href="mailto:hello@environ.digital"><FaEnvelope size="1em" /><span class="text"> hello@environ.digital</span></a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+          </div>
+        </div>
+      </div>
+    </footer>
       </footer>
     );
   }
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from "rsuite";
 
 require("./Contact.scss");
 
@@ -7,7 +8,68 @@ class Contact extends React.Component {
     return (
       <React.Fragment>
         <div className="contact">
-          <div className="contactform" />
+          <div className="contactform container content">
+          <form name="contact" method="POST" data-netlify="true">
+          <div class="field">
+  <label class="label">Name</label>
+  <div class="control">
+    <input class="input" type="text" placeholder="Name"/>
+  </div>
+</div>
+
+
+<div class="field">
+  <label class="label">Email</label>
+  <div class="control">
+    <input class="input" type="email" placeholder="Email"/>
+    <span class="icon is-small is-right">
+      <i class="fas fa-exclamation-triangle"></i>
+    </span>
+  </div>
+</div>
+
+<div class="field">
+  <label class="label">Subject</label>
+  <div class="control">
+    <div class="select">
+      <select>
+        <option>General enquiry</option>
+        <option>Website audit</option>
+        <option>Training request</option>
+      </select>
+    </div>
+  </div>
+</div>
+
+<div class="field">
+  <label class="label">Message</label>
+  <div class="control">
+    <textarea class="textarea" placeholder="Textarea"></textarea>
+  </div>
+</div>
+
+<div class="field">
+Would you like to receive tips and tricks for succeeding online?
+  <div class="control">
+    <label class="radio">
+      <input type="radio" name="tricks"/>
+      Yes
+    </label>
+    <div></div>
+    <label class="radio">
+      <input type="radio" name="tricks"/>
+      No
+    </label>
+  </div>
+</div>
+
+<div class="field">
+  <div class="control">
+    <Button class="is-link">Submit</Button>
+  </div>
+</div>
+</form>
+          </div>
         </div>
       </React.Fragment>
     );
