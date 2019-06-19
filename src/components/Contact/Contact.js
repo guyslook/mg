e0b2c6a80ceb2de09a53b8728aec2808965dc2a4
Aaron <azza1990uk@yahoo.com>
2019-06-19 01:17:20 +0100

    Add hidden form name input

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -11,6 +11,8 @@ class Contact extends React.Component {
           <div className="contactform container content">
 
           <form name="contact" method="POST" data-netlify="true">
+          <input type="hidden" name="form-name" value="contact" />
+
   <p>
     <label>Your Name: <input type="text" name="name" /></label>   
   </p>
