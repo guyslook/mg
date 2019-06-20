63ee54bf8474322f43c5f2fe7c4ea3b11c822d42
Aaron <azza1990uk@yahoo.com>
2019-06-20 01:26:45 +0100

    fix issue with element pages dropping too far low

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,10 @@ import AaBlockquote from "../components/AaBlockquote/AaBlockquote";
 
 const NotFoundPage = () => (
   <Layout>
-    <div className="element">
-      <AaBlockquote quote="Oops, this page doesn't exist, please contact us if you can't find what you're looking for" />
+    <div className="fourohfour">
+      <div className="element">
+        <AaBlockquote quote="Oops, this page doesn't exist, please contact us if you can't find what you're looking for" />
+      </div>
     </div>
   </Layout>
 );
